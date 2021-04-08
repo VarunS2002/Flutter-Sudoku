5d4b3446f07d8401d163200eb0d3ef2635370a5c
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-04-08 18:49:06 +0530

    Enabled Full-Screen (Immersive) mode

--- sudoku/lib/main.dart
+++ sudoku/lib/main.dart
@@ -14,6 +14,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setEnabledSystemUIOverlays([]);
   runApp(MyApp());
 }
 
