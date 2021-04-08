52d2da81dfe040230ef018759d75dba2b530cd00
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2021-04-08 18:50:28 +0530

    Added debugShowCheckedModeBanner parameter with default value

--- sudoku/lib/main.dart
+++ sudoku/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sudoku',
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Styles.primaryColor,
       ),
