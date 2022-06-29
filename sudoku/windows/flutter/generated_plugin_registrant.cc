bcb333c1c77d08ab8831278e94655754136c6903
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2022-06-29 22:59:29 +0530

    Updated pubspec.lock and generated Windows files

--- sudoku/windows/flutter/generated_plugin_registrant.cc
+++ sudoku/windows/flutter/generated_plugin_registrant.cc
@@ -2,14 +2,16 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
 #include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
-#include <url_launcher_windows/url_launcher_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   BitsdojoWindowPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
-  UrlLauncherPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
