03716a865511a2a5b6ff65db42926c843ac72bd5
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2022-06-30 17:53:30 +0530

    Set default desktop window size to 700x700

--- sudoku/windows/runner/main.cpp
+++ sudoku/windows/runner/main.cpp
@@ -30,7 +30,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(800, 800);
+  Win32Window::Size size(700, 700);
   if (!window.CreateAndShow(L"Sudoku", origin, size)) {
     return EXIT_FAILURE;
   }
