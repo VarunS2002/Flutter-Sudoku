659ad4a37a1bb691cfa6e8f0544877fa890e2697
VarunS2002 <30069380+VarunS2002@users.noreply.github.com>
2022-06-30 15:54:11 +0530

    Parameter has to be a string hex code instead of an instance of Color Removed wrapper class Updated material_color_generator.dart to fix all warnings

--- sudoku/lib/material_color_generator.dart
+++ sudoku/lib/material_color_generator.dart
@@ -1,32 +1,32 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
-class MaterialColorGenerator {
-  static MaterialColor createMaterialColor(Color color) {
-    List strengths = <double>[.05];
-    Map swatch = <int, Color>{};
-    final int r = color.red, g = color.green, b = color.blue;
+MaterialColor createMaterialColor(String hexColorCode) {
+  Color color = Color(int.parse("0xFF$hexColorCode"));
+  List strengths = <double>[.05];
+  Map swatch = <int, Color>{};
+  final int r = color.red, g = color.green, b = color.blue;
 
-    for (int i = 1; i < 10; i++) {
-      strengths.add(0.1 * i);
-    }
-    strengths.forEach((strength) {
-      final double ds = 0.5 - strength;
-      swatch[(strength * 1000).round()] = Color.fromRGBO(
-        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
-        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
-        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
-        1,
-      );
-    });
-    // print(color.value);
-    // print(swatch);
-    print("MaterialColor(${color.value},$swatch);");
-    return MaterialColor(color.value, swatch);
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
   }
+  for (var strength in strengths) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  }
+  // print(color.value);
+  // print(swatch);
+  print("MaterialColor(${color.value},$swatch);");
+  return MaterialColor(color.value, swatch);
 }
 
 void main() {
   // ignore: unused_local_variable
-  MaterialColor grey =
-      MaterialColorGenerator.createMaterialColor(Color(0xFF303030)); // 303030
+  MaterialColor grey = createMaterialColor("303030");
 }
